71563c4ccdefa82b25a4828f6780e1f697bc3438
storm <zhangqi.dev@qq.com>
2013-11-08 15:27:08 +0800

    Change activity package to ui

---
+++ src/com/storm/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.storm.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.storm.customloading.R;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
